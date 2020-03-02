f497fb589a8d0bc5fc2c054391cba804aeebc1d4
Dirk-Jan de Beijer <djjadebeijer@gmail.com>
2020-03-02 16:45:29 +0100

    Errors gefikst

--- fit-flex/src/app/app.module.ts
+++ fit-flex/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { OefeningComponent } from './components/oefening/oefening.component';
 import { OefeningPrestatieComponent } from './components/oefening-prestatie/oefening-prestatie.component';
 import { ReadMoreReadLessComponent } from './components/read-more-read-less/read-more-read-less.component';
 import { OefeningFormComponent } from './components/oefening-form/oefening-form.component';
-import { PresetatieFormComponent } from './components/presetatie-form/presetatie-form.component';
 import { PrestatieFormComponent } from './components/prestatie-form/prestatie-form.component';
 
 @NgModule({
@@ -18,7 +17,6 @@ import { PrestatieFormComponent } from './components/prestatie-form/prestatie-fo
     OefeningPrestatieComponent,
     ReadMoreReadLessComponent,
     OefeningFormComponent,
-    PresetatieFormComponent,
     PrestatieFormComponent
   ],
   imports: [
